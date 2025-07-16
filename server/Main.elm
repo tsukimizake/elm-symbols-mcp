fb22e0517de5aefca3b7cf400d0808c604572a93
tsukimizake <shomasd@gmail.com>
2025-07-16 12:28:10 +0900

    show types on the output

--- server/Main.elm
+++ server/Main.elm
@@ -1,11 +1,15 @@
-port module Main exposing (main)
+port module Main exposing (Model, Msg, main)
 
 import Elm.Parser as Parser
-import Elm.Syntax.Declaration as Declaration exposing (Declaration)
+import Elm.Syntax.Declaration as Declaration
 import Elm.Syntax.Exposing as Exposing
 import Elm.Syntax.File exposing (File)
 import Elm.Syntax.Module as Module
 import Elm.Syntax.Node as Node
+import Elm.Syntax.Signature as Signature
+import Elm.Syntax.Type as Type
+import Elm.Syntax.TypeAlias as TypeAlias
+import Elm.Syntax.TypeAnnotation as TypeAnnotation
 import Platform
 
 
@@ -52,6 +56,100 @@ type alias Decls =
     { funcs : List String, types : List String }
 
 
+formatTypeAnnotation : TypeAnnotation.TypeAnnotation -> String
+formatTypeAnnotation typeAnnotation =
+    case typeAnnotation of
+        TypeAnnotation.GenericType name ->
+            name
+
+        TypeAnnotation.Typed (Node.Node _ ( moduleName, name )) args ->
+            let
+                modulePart =
+                    if List.isEmpty moduleName then
+                        ""
+
+                    else
+                        String.join "." moduleName ++ "."
+
+                argsPart =
+                    if List.isEmpty args then
+                        ""
+
+                    else
+                        " " ++ String.join " " (List.map (Node.value >> formatTypeAnnotation) args)
+            in
+            modulePart ++ name ++ argsPart
+
+        TypeAnnotation.Unit ->
+            "()"
+
+        TypeAnnotation.Tupled items ->
+            "(" ++ String.join ", " (List.map (Node.value >> formatTypeAnnotation) items) ++ ")"
+
+        TypeAnnotation.Record fields ->
+            "{ " ++ String.join ", " (List.map formatRecordField fields) ++ " }"
+
+        TypeAnnotation.GenericRecord (Node.Node _ name) (Node.Node _ fields) ->
+            "{ " ++ name ++ " | " ++ String.join ", " (List.map formatRecordField fields) ++ " }"
+
+        TypeAnnotation.FunctionTypeAnnotation (Node.Node _ left) (Node.Node _ right) ->
+            formatTypeAnnotation left ++ " -> " ++ formatTypeAnnotation right
+
+
+formatRecordField : Node.Node ( Node.Node String, Node.Node TypeAnnotation.TypeAnnotation ) -> String
+formatRecordField (Node.Node _ ( Node.Node _ name, Node.Node _ typeAnnotation )) =
+    name ++ " : " ++ formatTypeAnnotation typeAnnotation
+
+
+formatFunctionSignature : Signature.Signature -> String
+formatFunctionSignature { name, typeAnnotation } =
+    Node.value name ++ " : " ++ formatTypeAnnotation (Node.value typeAnnotation)
+
+
+isExposedItem : String -> Node.Node Exposing.TopLevelExpose -> Bool
+isExposedItem targetName (Node.Node _ expose) =
+    case expose of
+        Exposing.InfixExpose _ ->
+            False
+
+        Exposing.FunctionExpose exposedName ->
+            exposedName == targetName
+
+        Exposing.TypeOrAliasExpose exposedName ->
+            exposedName == targetName
+
+        Exposing.TypeExpose { name } ->
+            name == targetName
+
+
+formatTypeAlias : TypeAlias.TypeAlias -> String
+formatTypeAlias { name, typeAnnotation } =
+    "type alias " ++ Node.value name ++ " = " ++ formatTypeAnnotation (Node.value typeAnnotation)
+
+
+formatCustomType : Type.Type -> String
+formatCustomType { name, constructors } =
+    let
+        constructorStrings =
+            List.map formatConstructor constructors
+                |> String.join " | "
+    in
+    "type " ++ Node.value name ++ " = " ++ constructorStrings
+
+
+formatConstructor : Node.Node Type.ValueConstructor -> String
+formatConstructor (Node.Node _ { name, arguments }) =
+    let
+        argsString =
+            if List.isEmpty arguments then
+                ""
+
+            else
+                " " ++ String.join " " (List.map (Node.value >> formatTypeAnnotation) arguments)
+    in
+    Node.value name ++ argsString
+
+
 formattedDecls : File -> String
 formattedDecls { declarations, moduleDefinition } =
     let
@@ -64,14 +162,17 @@ formattedDecls { declarations, moduleDefinition } =
                 |> List.foldl
                     (\decl acc ->
                         case decl of
-                            -- TODO nameだけでなくfunctionの型やtypeの右辺もelm-codegenでformatする
                             Declaration.FunctionDeclaration function ->
                                 function.signature
-                                    |> Maybe.map (Node.value >> .name >> Node.value)
+                                    |> Maybe.map Node.value
                                     |> Maybe.map
-                                        (\name ->
+                                        (\signature ->
+                                            let
+                                                name =
+                                                    Node.value signature.name
+                                            in
                                             if Exposing.exposesFunction name exposeList then
-                                                pushFunc name acc
+                                                pushFunc (formatFunctionSignature signature) acc
 
                                             else
                                                 acc
@@ -83,22 +184,32 @@ formattedDecls { declarations, moduleDefinition } =
                                     name =
                                         typeAlias.name |> Node.value
                                 in
-                                if Exposing.exposesFunction name exposeList then
-                                    pushType name acc
+                                case exposeList of
+                                    Exposing.All _ ->
+                                        pushType (formatTypeAlias typeAlias) acc
 
-                                else
-                                    acc
+                                    Exposing.Explicit items ->
+                                        if List.any (isExposedItem name) items then
+                                            pushType (formatTypeAlias typeAlias) acc
+
+                                        else
+                                            acc
 
                             Declaration.CustomTypeDeclaration typeDeclaration ->
                                 let
                                     name =
                                         typeDeclaration.name |> Node.value
                                 in
-                                if Exposing.exposesFunction name exposeList then
-                                    pushType name acc
+                                case exposeList of
+                                    Exposing.All _ ->
+                                        pushType (formatCustomType typeDeclaration) acc
 
-                                else
-                                    acc
+                                    Exposing.Explicit items ->
+                                        if List.any (isExposedItem name) items then
+                                            pushType (formatCustomType typeDeclaration) acc
+
+                                        else
+                                            acc
 
                             _ ->
                                 acc
@@ -107,11 +218,10 @@ formattedDecls { declarations, moduleDefinition } =
     in
     exposedDecls
         |> (\{ funcs, types } ->
-                "symbols: "
-                    ++ String.join ", " funcs
-                    ++ "\n"
-                    ++ "type decls: "
-                    ++ String.join ", " types
+                "symbols\n"
+                    ++ String.join "\n" funcs
+                    ++ "\n\ntype decls\n"
+                    ++ String.join "\n" types
            )
 
 
